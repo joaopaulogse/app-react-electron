9af58cc1cae2cfdaf6454c5a2bff5e5b1beb1c16
Joao Paulo <joaopaulogse@gmail.com>
2018-02-05 10:59:01 -0200

    init redux in project

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import React from "react"
 import { render } from "react-dom"
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+
+
 
 import App from './main/app'
+import reducers from './main/reducers'
 
-render(<App/>, document.getElementById("app"))
+
+render(
+    <Provider store={createStore(reducers)}>
+        <App />
+    </Provider>
+    , document.getElementById("app"))
